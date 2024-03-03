ae05bda64ca19f345b45f0272c9ea84312bea2a6
Your NameMorteza <mortezaalipour1777@gmail.com>
2024-03-04 01:33:35 +0330

    sidebar topbar mockdata cokors and theme added to project

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { createTheme } from "@mui/material/styles";
+
+
+// colors design tokens
+indigo: {
+    100: "#e0e0e0",
+    200: "#c2c2c2",
+    300: "#a3a3a3",
+    400: "#858585",
+    500: "#666666",
+    600: "#525252",
+    700: "#3d3d3d",
+    800: "#292929",
+    900: "#141414"
+},
+
+#141b2d
+#4cceac
+#db4f4a
